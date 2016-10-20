f6875013bf5a5374845a3cbc6b2d2e2372d65ca3
Vladik208 <Vladik208@gmail.com>
2016-10-20 23:05:51 +0300

    Update incorrectDataExceptions.hpp

--- src/incorrectDataExceptions.hpp
+++ src/incorrectDataExceptions.hpp
@@ -6,6 +6,6 @@ class IncorrectDataExceptions : public std::invalid_argument
 public:
   IncorrectDataExceptions() = default;
   IncorrectDataExceptions(std::string const & message):std::invalid_argument(message) {};
-  const char* what() const noexcept { /*будт своя обработка*/ return std::invalid_argument::what(); }
+  const char* what() const noexcept { /*будет своя обработка*/ return std::invalid_argument::what(); }
   ~IncorrectDataExceptions(){};
 };
