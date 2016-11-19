ab2ed3143f5a0bc6332f866bc43d8ac22715e214
Vladik208 <Vladik208@gmail.com>
2016-11-19 22:13:03 +0300

    Домашнее задание 6

---
+++ src/factory.hpp
@@ -0,0 +1,8 @@
+class Factory
+{
+public:
+  template<typename T, typename... Args> std::unique_ptr<T> Create(Args && ... args)
+  {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  }
+};
