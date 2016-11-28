b53f3b509882e9169b38f398f57a80adebd80e3a
Vladik208 <Vladik208@gmail.com>
2016-11-29 00:42:59 +0300

    Домашнее задание 9

--- src/settings_window.cpp
+++ src/settings_window.cpp
@@ -1,38 +1,31 @@
 #pragma once
 #include "settings_window.hpp"
-#include <QMainWindow>
-#include <QTimer>
-#include <QGridLayout>
-#include <QOpenGLWidget>
-#include <QPushButton>
-#include <QMessageBox>
-#include "gl_widget.hpp"
-#include <QApplication>
-#include <QVBoxLayout>
-#include <QHBoxLayout>
-#include <QWidget>
-#include <QComboBox>
+
+#include <fstream>
+//#include "C:\Users\Max\Desktop\template-window_demo\template-window_demo\3party\pugixml\src\pugixml.hpp"
 typedef void (QWidget::*QWidgetVoidSlot)();
 
 SettingsWindow::SettingsWindow()
 {
-  QWidget * centralWidget = new QWidget(this);
+  centralWidget = new QWidget(this);
   setCentralWidget(centralWidget);
-  QCheckBox *check1 = new QCheckBox("Пули-Шмули");
-  QCheckBox *check2 = new QCheckBox("Питер");
-  QCheckBox *check3 = new QCheckBox("VR");
+  checkIsObstacles = new QCheckBox("Препятсвия");
+  checkMovableAliens = new QCheckBox("Инопланетяне движутся");
+  checkSound = new QCheckBox("Звук");
   QGridLayout * layout = new QGridLayout(centralWidget);
 
-  QComboBox * comboBox = new QComboBox(this);
-  comboBox->addItem("Легко", 1);
-  comboBox->addItem("Средне", 2);
-  comboBox->addItem("Сложно", 3);
-  comboBox->setCurrentIndex(1);
-  connect(comboBox, SIGNAL(activated(int)), this, SLOT(setDifficulty(int)));
+  comboBoxDifficulty = new QComboBox(this);
+  comboBoxDifficulty->addItem("Легко", 1);
+  comboBoxDifficulty->addItem("Средне", 2);
+  comboBoxDifficulty->addItem("Сложно", 3);
+  comboBoxDifficulty->setCurrentIndex(1);
+  connect(comboBoxDifficulty, SIGNAL(activated(int)), this, SLOT(setDifficulty(int)));
 
   QPushButton *cancel =new QPushButton("Отмена");
-  QPushButton *def = new QPushButton("Вернуть стандартные");
-  QPushButton *save =new QPushButton("Сохранить");
+  QPushButton *def = new QPushButton("Вернуть состояние");
+  QPushButton *save =new QPushButton("Сохранить состояние");
+  connect(save,SIGNAL(clicked()),this,SLOT(WriteJson()));
+  connect(def,SIGNAL(clicked()),this,SLOT(ReadJson()));
 
   QWidget *bottom_widget = new QWidget(this);
   QHBoxLayout *bottom = new QHBoxLayout(bottom_widget);
@@ -40,27 +33,63 @@ SettingsWindow::SettingsWindow()
   bottom->addWidget(def);
   bottom->addWidget(save);
 
-  layout->addWidget(comboBox,0,0);
-  layout->addWidget(check1,0,1);
-  layout->addWidget(check2,1,1);
-  layout->addWidget(check3,2,1);
+  layout->addWidget(comboBoxDifficulty,0,0);
+  layout->addWidget(checkIsObstacles,0,1);
+  layout->addWidget(checkMovableAliens,1,1);
+  layout->addWidget(checkSound,2,1);
   layout->addWidget(bottom_widget,3,0);
 
 
   setFocusPolicy(Qt::StrongFocus);
   show();
+  ReadJson();
 }
 
 SettingsWindow::setDifficulty(int m_difficulty)
 {
-  QMessageBox msgBox;
-  msgBox.setWindowTitle("Hello");
-  if(m_difficulty == 0)
-  msgBox.setText("0");
-  if(m_difficulty == 1)
-  msgBox.setText("1");
-  if(m_difficulty == 2)
-  msgBox.setText("2");
-  msgBox.resize(500,500);
-  msgBox.exec();
+  //здесь будет код
 }
+
+void SettingsWindow::WriteJson()
+{
+  Json::Value settings;
+
+  auto & root = settings["settings"];
+  root["difficulty"] = comboBoxDifficulty->currentIndex();
+  root["is_obstacles"] = checkIsObstacles->isChecked();
+  root["movable_aliens"] = checkMovableAliens->isChecked();
+  root["sound"] = checkSound->isChecked();
+
+  std::ofstream settingsFile;
+  settingsFile.open("settings.json");
+  if (settingsFile.is_open())
+  {
+    Json::StyledWriter styledWriter;
+    settingsFile << styledWriter.write(settings);
+    settingsFile.close();
+  }
+}
+void SettingsWindow::ReadJson()
+{
+  Json::Value settings;
+  std::ifstream file("settings.json");
+  if (file.is_open())
+  {
+    file >> settings;
+    file.close();
+  }
+  comboBoxDifficulty->setCurrentIndex(settings["settings"]["difficulty"].asInt());
+  if (settings["settings"]["is_obstacles"].asBool())
+      checkIsObstacles->setCheckState(Qt::Checked);
+  else
+      checkIsObstacles->setCheckState(Qt::Unchecked);
+  if (settings["settings"]["movable_aliens"].asBool())
+      checkMovableAliens->setCheckState(Qt::Checked);
+  else
+      checkMovableAliens->setCheckState(Qt::Unchecked);
+  if (settings["settings"]["sound"].asBool())
+      checkSound->setCheckState(Qt::Checked);
+  else
+      checkSound->setCheckState(Qt::Unchecked);
+}
+
